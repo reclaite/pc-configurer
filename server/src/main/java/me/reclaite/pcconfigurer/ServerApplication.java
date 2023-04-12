ce4dcfd64c46cdf1fb269ff46724169ee523eddc
Alexander Gordienko <reclaite@gmail.com>
2023-04-12 21:28:47 +0300

    Initial commit

---
+++ server/src/main/java/me/reclaite/pcconfigurer/ServerApplication.java
@@ -0,0 +1,13 @@
+package me.reclaite.pcconfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+    
+}
